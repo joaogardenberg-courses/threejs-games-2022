40115e54d67995f2fcaed06aaef0969231b69dba
João Gardenberg <joaogardenberg@gmail.com>
2022-02-17 15:59:39 +0100

    Lecture 3_2

--- start/lecture3_2/app.js
+++ start/lecture3_2/app.js
@@ -1,21 +1,16 @@
-import * as THREE from '../../libs/three126/three.module.js';
-import { OrbitControls } from '../../libs/three126/OrbitControls.js';
+import * as THREE from '../../libs/three126/three.module.js'
+import { OrbitControls } from '../../libs/three126/OrbitControls.js'
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-    
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
-        
-    }
-    
-	render( ) {   
-        
-    }
+class App {
+  constructor() {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+    window.addEventListener('resize', this.resize.bind(this))
+  }
+
+  resize() {}
+
+  render() {}
 }
 
-export { App };
+export { App }
