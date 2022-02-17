be74053884bfa7ae0a8e0ab6cd7bb68d6e6d5c29
João Gardenberg <joaogardenberg@gmail.com>
2022-02-17 18:45:18 +0100

    Lesson 4_4

--- start/lecture4_4/Game.js
+++ start/lecture4_4/Game.js
@@ -1,93 +1,164 @@
-import * as THREE from '../../libs/three137/three.module.js';
-import { GLTFLoader } from '../../libs/three137/GLTFLoader.js';
-import { DRACOLoader } from '../../libs/three137/DRACOLoader.js';
-import { RGBELoader } from '../../libs/three137/RGBELoader.js';
-import { OrbitControls } from '../../libs/three137/OrbitControls.js';
-import { LoadingBar } from '../../libs/LoadingBar.js';
+import * as THREE from '../../libs/three137/three.module.js'
+import { GLTFLoader } from '../../libs/three137/GLTFLoader.js'
+import { DRACOLoader } from '../../libs/three137/DRACOLoader.js'
+import { RGBELoader } from '../../libs/three137/RGBELoader.js'
+import { OrbitControls } from '../../libs/three137/OrbitControls.js'
+import { LoadingBar } from '../../libs/LoadingBar.js'
 
-class Game{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-        
-		this.clock = new THREE.Clock();
+class Game {
+  constructor() {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
 
-        this.loadingBar = new LoadingBar();
-        this.loadingBar.visible = false;
+    this.clock = new THREE.Clock()
 
-		this.assetsPath = '../../assets/';
-        
-		this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 50 );
-		this.camera.position.set( 1, 1.7, 2.8 );
-        
-		let col = 0x605550;
-		this.scene = new THREE.Scene();
-		this.scene.background = new THREE.Color( col );
-		
-		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-		this.scene.add(ambient);
+    this.loadingBar = new LoadingBar()
+    this.loadingBar.visible = false
 
-        const light = new THREE.DirectionalLight();
-        light.position.set( 0.2, 1, 1 );
-			
-		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
-		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-		container.appendChild( this.renderer.domElement );
-        this.setEnvironment();
-        
-        const controls = new OrbitControls( this.camera, this.renderer.domElement );
-        controls.target.set(0, 1, 0);
-		controls.update();
+    this.assetsPath = '../../assets/'
 
-        this.loadEve();
-		
-		window.addEventListener('resize', this.resize.bind(this) );
-        
-	}
-	
-    resize(){
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-    	this.camera.updateProjectionMatrix();
-    	this.renderer.setSize( window.innerWidth, window.innerHeight ); 
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      50
+    )
+    this.camera.position.set(1, 1.7, 2.8)
+
+    let col = 0x605550
+    this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(col)
+
+    const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1)
+    this.scene.add(ambient)
+
+    const light = new THREE.DirectionalLight()
+    light.position.set(0.2, 1, 1)
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.outputEncoding = THREE.sRGBEncoding
+    container.appendChild(this.renderer.domElement)
+    this.setEnvironment()
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement)
+    controls.target.set(0, 1, 0)
+    controls.update()
+
+    this.loadEve()
+
+    window.addEventListener('resize', this.resize.bind(this))
+  }
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+
+  setEnvironment() {
+    const loader = new RGBELoader().setPath(this.assetsPath)
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer)
+    pmremGenerator.compileEquirectangularShader()
+
+    const self = this
+
+    loader.load(
+      'hdr/factory.hdr',
+      (texture) => {
+        const envMap = pmremGenerator.fromEquirectangular(texture).texture
+        pmremGenerator.dispose()
+
+        self.scene.environment = envMap
+      },
+      undefined,
+      (err) => {
+        console.error(err.message)
+      }
+    )
+  }
+
+  loadEve() {
+    const loader = new GLTFLoader().setPath(`${this.assetsPath}factory/`)
+    const dracoLoader = new DRACOLoader()
+    dracoLoader.setDecoderPath('../../libs/three128/draco/')
+    loader.setDRACOLoader(dracoLoader)
+
+    this.loadingBar.visible = true
+
+    loader.load(
+      'eve.glb',
+      (gltf) => {
+        this.eve = gltf.scene
+        this.scene.add(this.eve)
+        this.mixer = new THREE.AnimationMixer(this.eve)
+        this.animations = {}
+
+        gltf.animations.forEach(
+          (animation) =>
+            (this.animations[animation.name.toLowerCase()] = animation)
+        )
+
+        this.actionName = ''
+        this.newAnim()
+        this.loadingBar.visible = false
+        this.renderer.setAnimationLoop(this.render.bind(this))
+      },
+      ({ loader, total }) => (this.loadingBar.progress = loader / total),
+      (err) => console.error(err)
+    )
+  }
+
+  newAnim() {
+    const keys = Object.keys(this.animations)
+    let index
+
+    do {
+      index = Math.floor(Math.random() * keys.length)
+    } while (keys[index] === this.actionName)
+
+    this.action = keys[index]
+
+    setTimeout(this.newAnim.bind(this), 3000)
+  }
+
+  set action(name) {
+    const clip = this.animations[name]
+
+    if (clip !== undefined) {
+      const action = this.mixer.clipAction(clip)
+      action.reset()
+
+      if (name === 'shot') {
+        action.setLoop(THREE.LoopOnce)
+        action.clampWhenFinished = true
+      }
+
+      action.play()
+
+      if (this.currAction) {
+        if (this.actionName === 'shot') {
+          this.currAction.enabled = false
+        }
+
+        this.currAction.crossFadeTo(action, 0.5)
+      }
+
+      this.actionName = name
+      this.currAction = action
     }
-    
-    setEnvironment(){
-        const loader = new RGBELoader().setPath(this.assetsPath);
-        const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
-        pmremGenerator.compileEquirectangularShader();
-        
-        const self = this;
-        
-        loader.load( 'hdr/factory.hdr', ( texture ) => {
-          const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-          pmremGenerator.dispose();
+  }
 
-          self.scene.environment = envMap;
+  render() {
+    const dt = this.clock.getDelta()
 
-        }, undefined, (err)=>{
-            console.error( err.message );
-        } );
+    if (this.mixer !== undefined) {
+      this.mixer.update(dt)
     }
 
-    loadEve(){
-    	
-	}			
-    
-	newAnim(){
-		
-	}
-
-	set action(name){
-		
-	}
-
-	render() {
-		const dt = this.clock.getDelta();
-
-        this.renderer.render( this.scene, this.camera );
-    }
+    this.renderer.render(this.scene, this.camera)
+  }
 }
 
-export { Game };
+export { Game }
