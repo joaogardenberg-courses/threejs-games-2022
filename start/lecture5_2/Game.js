dc2d3a8a8309d98203868000201b28459c237dd6
João Gardenberg <joaogardenberg@gmail.com>
2022-02-17 23:19:03 +0100

    Lesson 5_2

--- start/lecture5_2/Game.js
+++ start/lecture5_2/Game.js
@@ -1,123 +1,165 @@
-import * as THREE from '../../libs/three137/three.module.js';
-import { RGBELoader } from '../../libs/three137/RGBELoader.js';
-import { LoadingBar } from '../../libs/LoadingBar.js';
-import { Plane } from './Plane.js';
+import * as THREE from '../../libs/three137/three.module.js'
+import { RGBELoader } from '../../libs/three137/RGBELoader.js'
+import { LoadingBar } from '../../libs/LoadingBar.js'
+import { Plane } from './Plane.js'
 
-class Game{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-        
-        this.loadingBar = new LoadingBar();
-        this.loadingBar.visible = false;
+class Game {
+  constructor() {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
 
-        this.clock = new THREE.Clock();
+    this.loadingBar = new LoadingBar()
+    this.loadingBar.visible = false
 
-		this.assetsPath = '../../assets/';
-        
-		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
-        this.camera.position.set( -4.37, 0, -4.75 );
-        this.camera.lookAt(0, 0, 6);
+    this.clock = new THREE.Clock()
 
-        this.cameraController = new THREE.Object3D();
-        this.cameraController.add(this.camera);
-        this.cameraTarget = new THREE.Vector3(0,0,6);
-        
-		this.scene = new THREE.Scene();
-        this.scene.add(this.cameraController);
+    this.assetsPath = '../../assets/'
 
-		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-        ambient.position.set( 0.5, 1, 0.25 );
-		this.scene.add(ambient);
-			
-		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
-		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-		container.appendChild( this.renderer.domElement );
-        this.setEnvironment();
-        
-        this.load();
-	}
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      100
+    )
+    this.camera.position.set(-4.37, 0, -4.75)
+    this.camera.lookAt(0, 0, 6)
 
-    startGame(){
-        
+    this.cameraController = new THREE.Object3D()
+    this.cameraController.add(this.camera)
+    this.cameraTarget = new THREE.Vector3(0, 0, 6)
+
+    this.scene = new THREE.Scene()
+    this.scene.add(this.cameraController)
+
+    const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1)
+    ambient.position.set(0.5, 1, 0.25)
+    this.scene.add(ambient)
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.outputEncoding = THREE.sRGBEncoding
+    container.appendChild(this.renderer.domElement)
+    this.setEnvironment()
+
+    this.load()
+
+    document.addEventListener('keydown', this.keyDown.bind(this))
+    document.addEventListener('keyup', this.keyUp.bind(this))
+
+    document.addEventListener('mousedown', this.mouseDown.bind(this))
+    document.addEventListener('mouseup', this.mouseUp.bind(this))
+    document.addEventListener('touchstart', this.mouseDown.bind(this))
+    document.addEventListener('touchend', this.mouseUp.bind(this))
+
+    this.spaceKey = false
+    this.active = false
+
+    const btn = document.getElementById('playBtn')
+    btn.addEventListener('click', this.startGame.bind(this))
+  }
+
+  startGame() {
+    const instructions = document.getElementById('instructions')
+    const btn = document.getElementById('playBtn')
+    instructions.style.display = 'none'
+    btn.style.display = 'none'
+    this.active = true
+  }
+
+  mouseDown() {
+    this.spaceKey = true
+  }
+
+  mouseUp() {
+    this.spaceKey = false
+  }
+
+  keyDown(evt) {
+    if (evt.keyCode === 32) {
+      this.spaceKey = true
     }
+  }
 
-    resize(){
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-    	this.camera.updateProjectionMatrix();
-    	this.renderer.setSize( window.innerWidth, window.innerHeight ); 
+  keyUp(evt) {
+    if (evt.keyCode === 32) {
+      this.spaceKey = false
     }
+  }
 
-    setEnvironment(){
-        const loader = new RGBELoader().setPath(this.assetsPath);
-        const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
-        pmremGenerator.compileEquirectangularShader();
-        
-        const self = this;
-        
-        loader.load( 'hdr/venice_sunset_1k.hdr', ( texture ) => {
-          const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-          pmremGenerator.dispose();
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+  }
 
-          self.scene.environment = envMap;
+  setEnvironment() {
+    const loader = new RGBELoader().setPath(this.assetsPath)
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer)
+    pmremGenerator.compileEquirectangularShader()
 
-        }, undefined, (err)=>{
-            console.error( err.message );
-        } );
-    }
-    
-	load(){
-        this.loadSkybox();
-        this.loading = true;
-        this.loadingBar.visible = true;
+    const self = this
 
-        this.plane = new Plane(this);
-    }
+    loader.load(
+      'hdr/venice_sunset_1k.hdr',
+      (texture) => {
+        const envMap = pmremGenerator.fromEquirectangular(texture).texture
+        pmremGenerator.dispose()
 
-    loadSkybox(){
-        this.scene.background = new THREE.CubeTextureLoader()
-	        .setPath( `${this.assetsPath}plane/paintedsky/` )
-            .load( [
-                'px.jpg',
-                'nx.jpg',
-                'py.jpg',
-                'ny.jpg',
-                'pz.jpg',
-                'nz.jpg'
-            ], () => {
-                this.renderer.setAnimationLoop(this.render.bind(this));
-            } );
-    }		
+        self.scene.environment = envMap
+      },
+      undefined,
+      (err) => {
+        console.error(err.message)
+      }
+    )
+  }
 
-    updateCamera(){
-        this.cameraController.position.copy( this.plane.position );
-        this.cameraController.position.y = 0;
-        this.cameraTarget.copy(this.plane.position);
-        this.cameraTarget.z += 6;
-        this.camera.lookAt( this.cameraTarget );
-    }
+  load() {
+    this.loadSkybox()
+    this.loading = true
+    this.loadingBar.visible = true
 
-	render() {
-        if (this.loading){
-            if (this.plane.ready){
-                this.loading = false;
-                this.loadingBar.visible = false;
-            }else{
-                return;
-            }
+    this.plane = new Plane(this)
+  }
+
+  loadSkybox() {
+    this.scene.background = new THREE.CubeTextureLoader()
+      .setPath(`${this.assetsPath}plane/paintedsky/`)
+      .load(
+        ['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg'],
+        () => {
+          this.renderer.setAnimationLoop(this.render.bind(this))
         }
+      )
+  }
 
-        const time = this.clock.getElapsedTime();
+  updateCamera() {
+    this.cameraController.position.copy(this.plane.position)
+    this.cameraController.position.y = 0
+    this.cameraTarget.copy(this.plane.position)
+    this.cameraTarget.z += 6
+    this.camera.lookAt(this.cameraTarget)
+  }
 
-        this.plane.update(time);
-    
-        this.updateCamera();
-    
-        this.renderer.render( this.scene, this.camera );
-
+  render() {
+    if (this.loading) {
+      if (this.plane.ready) {
+        this.loading = false
+        this.loadingBar.visible = false
+      } else {
+        return
+      }
     }
+
+    const time = this.clock.getElapsedTime()
+
+    this.plane.update(time)
+
+    this.updateCamera()
+
+    this.renderer.render(this.scene, this.camera)
+  }
 }
 
-export { Game };
+export { Game }
