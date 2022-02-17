0cb9305ebbe11a4f36f32d6400cd565f773fd880
João Gardenberg <joaogardenberg@gmail.com>
2022-02-17 15:59:48 +0100

    Lecture 3_3

--- start/lecture3_3/app.js
+++ start/lecture3_3/app.js
@@ -1,21 +1,37 @@
-import * as THREE from '../../libs/three126/three.module.js';
-import { OrbitControls } from '../../libs/three126/OrbitControls.js';
+import * as THREE from '../../libs/three126/three.module.js'
+import { OrbitControls } from '../../libs/three126/OrbitControls.js'
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
+class App {
+  constructor() {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
 
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
+    this.camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    )
+    this.camera.position.set(0, 0, 4)
 
-    }
-    
-	render( ) {   
-		
-    }
+    this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(0xaaaaaa)
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    container.appendChild(this.renderer.domElement)
+
+    this.renderer.setAnimationLoop(this.render)
+
+    window.addEventListener('resize', this.resize)
+  }
+
+  resize = () => {}
+
+  render = () => {
+    this.renderer.render(this.scene, this.camera)
+  }
 }
 
-export { App };
+export { App }
