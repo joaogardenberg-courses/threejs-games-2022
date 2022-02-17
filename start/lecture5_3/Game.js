652b07e187afb618a5268a59a686559534ac0afb
João Gardenberg <joaogardenberg@gmail.com>
2022-02-17 23:40:14 +0100

    Lesson 5_3

--- start/lecture5_3/Game.js
+++ start/lecture5_3/Game.js
@@ -1,182 +1,214 @@
-import * as THREE from '../../libs/three137/three.module.js';
-import { RGBELoader } from '../../libs/three137/RGBELoader.js';
-import { LoadingBar } from '../../libs/LoadingBar.js';
-import { Plane } from './Plane.js';
-import { Obstacles } from './Obstacles.js';
+import * as THREE from '../../libs/three137/three.module.js'
+import { RGBELoader } from '../../libs/three137/RGBELoader.js'
+import { LoadingBar } from '../../libs/LoadingBar.js'
+import { Plane } from './Plane.js'
+import { Obstacles } from './Obstacles.js'
 
-class Game{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-        
-        this.loadingBar = new LoadingBar();
-        this.loadingBar.visible = false;
+class Game {
+  constructor() {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
 
-        this.clock = new THREE.Clock();
+    this.loadingBar = new LoadingBar()
+    this.loadingBar.visible = false
 
-		this.assetsPath = '../../assets/';
-        
-		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
-        this.camera.position.set( -4.37, 0, -4.75 );
-        this.camera.lookAt(0, 0, 6);
+    this.clock = new THREE.Clock()
 
-        this.cameraController = new THREE.Object3D();
-        this.cameraController.add(this.camera);
-        this.cameraTarget = new THREE.Vector3(0,0,6);
-        
-		this.scene = new THREE.Scene();
-        this.scene.add(this.cameraController);
+    this.assetsPath = '../../assets/'
 
-		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-        ambient.position.set( 0.5, 1, 0.25 );
-		this.scene.add(ambient);
-			
-		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
-		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-		container.appendChild( this.renderer.domElement );
-        this.setEnvironment();
-        
-        this.active = false;
-        this.load();
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      100
+    )
+    this.camera.position.set(-4.37, 0, -4.75)
+    this.camera.lookAt(0, 0, 6)
 
-        window.addEventListener('resize', this.resize.bind(this) );
+    this.cameraController = new THREE.Object3D()
+    this.cameraController.add(this.camera)
+    this.cameraTarget = new THREE.Vector3(0, 0, 6)
 
-        document.addEventListener('keydown', this.keyDown.bind(this));
-        document.addEventListener('keyup', this.keyUp.bind(this));
+    this.scene = new THREE.Scene()
+    this.scene.add(this.cameraController)
 
-        document.addEventListener('touchstart', this.mouseDown.bind(this) );
-        document.addEventListener('touchend', this.mouseUp.bind(this) );
-        document.addEventListener('mousedown', this.mouseDown.bind(this) );
-        document.addEventListener('mouseup', this.mouseUp.bind(this) );
-        
-        this.spaceKey = false;
+    const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1)
+    ambient.position.set(0.5, 1, 0.25)
+    this.scene.add(ambient)
 
-        const btn = document.getElementById('playBtn');
-        btn.addEventListener('click', this.startGame.bind(this));
-	}
-	
-    startGame(){
-        const instructions = document.getElementById('instructions');
-        const btn = document.getElementById('playBtn');
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.outputEncoding = THREE.sRGBEncoding
+    container.appendChild(this.renderer.domElement)
+    this.setEnvironment()
 
-        instructions.style.display = 'none';
-        btn.style.display = 'none';
+    this.active = false
+    this.load()
 
-        this.active = true;
+    window.addEventListener('resize', this.resize.bind(this))
+
+    document.addEventListener('keydown', this.keyDown.bind(this))
+    document.addEventListener('keyup', this.keyUp.bind(this))
+
+    document.addEventListener('touchstart', this.mouseDown.bind(this))
+    document.addEventListener('touchend', this.mouseUp.bind(this))
+    document.addEventListener('mousedown', this.mouseDown.bind(this))
+    document.addEventListener('mouseup', this.mouseUp.bind(this))
+
+    this.spaceKey = false
+
+    const btn = document.getElementById('playBtn')
+    btn.addEventListener('click', this.startGame.bind(this))
+  }
+
+  startGame() {
+    const gameOver = document.getElementById('gameover')
+    const instructions = document.getElementById('instructions')
+    const btn = document.getElementById('playBtn')
+
+    gameOver.style.display = 'none'
+    instructions.style.display = 'none'
+    btn.style.display = 'none'
+
+    this.score = 0
+    this.lives = 3
+
+    const score = document.getElementById('score')
+    score.innerHTML = this.score
+
+    const lives = document.getElementById('lives')
+    lives.innerHTML = this.lives
+
+    this.active = true
+  }
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+
+  keyDown(evt) {
+    switch (evt.keyCode) {
+      case 32:
+        this.spaceKey = true
+        break
     }
+  }
 
-    resize(){
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-    	this.camera.updateProjectionMatrix();
-    	this.renderer.setSize( window.innerWidth, window.innerHeight ); 
+  keyUp(evt) {
+    switch (evt.keyCode) {
+      case 32:
+        this.spaceKey = false
+        break
     }
+  }
 
-    keyDown(evt){
-        switch(evt.keyCode){
-            case 32:
-                this.spaceKey = true; 
-                break;
+  mouseDown(evt) {
+    this.spaceKey = true
+  }
+
+  mouseUp(evt) {
+    this.spaceKey = false
+  }
+
+  setEnvironment() {
+    const loader = new RGBELoader().setPath(this.assetsPath)
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer)
+    pmremGenerator.compileEquirectangularShader()
+
+    const self = this
+
+    loader.load(
+      'hdr/venice_sunset_1k.hdr',
+      (texture) => {
+        const envMap = pmremGenerator.fromEquirectangular(texture).texture
+        pmremGenerator.dispose()
+
+        self.scene.environment = envMap
+      },
+      undefined,
+      (err) => {
+        console.error(err.message)
+      }
+    )
+  }
+
+  load() {
+    this.loadSkybox()
+    this.loading = true
+    this.loadingBar.visible = true
+
+    this.plane = new Plane(this)
+    this.obstacles = new Obstacles(this)
+  }
+
+  loadSkybox() {
+    this.scene.background = new THREE.CubeTextureLoader()
+      .setPath(`${this.assetsPath}plane/paintedsky/`)
+      .load(
+        ['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg'],
+        () => {
+          this.renderer.setAnimationLoop(this.render.bind(this))
         }
-    }
-    
-    keyUp(evt){
-        switch(evt.keyCode){
-            case 32:
-                this.spaceKey = false;
-                break;
-        }
-    }
+      )
+  }
 
-    mouseDown(evt){
-        this.spaceKey = true;
+  gameOver() {
+    this.active = false
+    const gameOver = document.getElementById('gameover')
+    const btn = document.getElementById('playBtn')
+    gameOver.style.display = 'block'
+    btn.style.display = 'block'
+  }
+
+  incScore() {
+    this.score++
+    const score = document.getElementById('score')
+    score.innerHTML = this.score
+  }
+
+  decLives() {
+    this.lives--
+    const lives = document.getElementById('lives')
+    lives.innerHTML = this.lives
+
+    if (lives <= 0) {
+      this.gameOver()
     }
+  }
 
-    mouseUp(evt){
-        this.spaceKey = false;
-    }
+  updateCamera() {
+    this.cameraController.position.copy(this.plane.position)
+    this.cameraController.position.y = 0
+    this.cameraTarget.copy(this.plane.position)
+    this.cameraTarget.z += 6
+    this.camera.lookAt(this.cameraTarget)
+  }
 
-    setEnvironment(){
-        const loader = new RGBELoader().setPath(this.assetsPath);
-        const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
-        pmremGenerator.compileEquirectangularShader();
-        
-        const self = this;
-        
-        loader.load( 'hdr/venice_sunset_1k.hdr', ( texture ) => {
-          const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-          pmremGenerator.dispose();
-
-          self.scene.environment = envMap;
-
-        }, undefined, (err)=>{
-            console.error( err.message );
-        } );
-    }
-    
-	load(){
-        this.loadSkybox();
-        this.loading = true;
-        this.loadingBar.visible = true;
-
-        this.plane = new Plane(this);
-    }
-
-    loadSkybox(){
-        this.scene.background = new THREE.CubeTextureLoader()
-	        .setPath( `${this.assetsPath}plane/paintedsky/` )
-            .load( [
-                'px.jpg',
-                'nx.jpg',
-                'py.jpg',
-                'ny.jpg',
-                'pz.jpg',
-                'nz.jpg'
-            ], () => {
-                this.renderer.setAnimationLoop(this.render.bind(this));
-            } );
-    }
-    
-    gameOver(){
-        
+  render() {
+    if (this.loading) {
+      if (this.plane.ready && this.obstacles.ready) {
+        this.loading = false
+        this.loadingBar.visible = false
+      } else {
+        return
+      }
     }
 
-    incScore(){
-        
+    const time = this.clock.getElapsedTime()
+
+    if (this.active) {
+      this.obstacles.update(this.plane.position)
     }
 
-    decLives(){
-       
-    }
+    this.plane.update(time)
 
-    updateCamera(){
-        this.cameraController.position.copy( this.plane.position );
-        this.cameraController.position.y = 0;
-        this.cameraTarget.copy(this.plane.position);
-        this.cameraTarget.z += 6;
-        this.camera.lookAt( this.cameraTarget );
-    }
+    this.updateCamera()
 
-	render() {
-        if (this.loading){
-            if (this.plane.ready){
-                this.loading = false;
-                this.loadingBar.visible = false;
-            }else{
-                return;
-            }
-        }
-
-        const time = this.clock.getElapsedTime();
-
-        this.plane.update(time);
-    
-        this.updateCamera();
-    
-        this.renderer.render( this.scene, this.camera );
-
-    }
+    this.renderer.render(this.scene, this.camera)
+  }
 }
 
-export { Game };
+export { Game }
--- start/lecture5_3/Obstacles.js
+++ start/lecture5_3/Obstacles.js
@@ -1,99 +1,121 @@
-import { Group, Vector3 } from '../../libs/three137/three.module.js';
-import { GLTFLoader } from '../../libs/three137/GLTFLoader.js';
+import { Group, Vector3 } from '../../libs/three137/three.module.js'
+import { GLTFLoader } from '../../libs/three137/GLTFLoader.js'
 
-class Obstacles{
-    constructor(game){
-        this.assetsPath = game.assetsPath;
-        this.loadingBar = game.loadingBar;
-		this.game = game;
-		this.scene = game.scene;
-        this.loadStar();
-		this.loadBomb();
-		this.tmpPos = new Vector3();
+class Obstacles {
+  constructor(game) {
+    this.assetsPath = game.assetsPath
+    this.loadingBar = game.loadingBar
+    this.game = game
+    this.scene = game.scene
+    this.loadStar()
+    this.loadBomb()
+    this.tmpPos = new Vector3()
+  }
+
+  loadStar() {
+    const loader = new GLTFLoader().setPath(`${this.assetsPath}plane/`)
+    this.ready = false
+
+    // Load a glTF resource
+    loader.load(
+      // resource URL
+      'star.glb',
+      // called when the resource is loaded
+      (gltf) => {
+        this.star = gltf.scene.children[0]
+
+        this.star.name = 'star'
+
+        if (this.bomb !== undefined) this.initialize()
+      },
+      // called while loading is progressing
+      (xhr) => {
+        this.loadingBar.update('star', xhr.loaded, xhr.total)
+      },
+      // called when loading has errors
+      (err) => {
+        console.error(err)
+      }
+    )
+  }
+
+  loadBomb() {
+    const loader = new GLTFLoader().setPath(`${this.assetsPath}plane/`)
+
+    // Load a glTF resource
+    loader.load(
+      // resource URL
+      'bomb.glb',
+      // called when the resource is loaded
+      (gltf) => {
+        this.bomb = gltf.scene.children[0]
+
+        if (this.star !== undefined) this.initialize()
+      },
+      // called while loading is progressing
+      (xhr) => {
+        this.loadingBar.update('bomb', xhr.loaded, xhr.total)
+      },
+      // called when loading has errors
+      (err) => {
+        console.error(err)
+      }
+    )
+  }
+
+  initialize() {
+    this.obstacles = []
+    const obstacle = new Group()
+    obstacle.add(this.star)
+    this.bomb.rotation.x = -Math.PI / 2
+    this.bomb.position.y = 7.5
+    obstacle.add(this.bomb)
+    let rotate = true
+
+    for (let y = 5; y > -8; y -= 2.5) {
+      rotate = !rotate
+
+      if (y === 0) {
+        continue
+      }
+
+      const bomb = this.bomb.clone()
+      bomb.rotation.x = rotate ? -Math.PI / 2 : 0
+      bomb.position.y = y
+      obstacle.add(bomb)
     }
 
-    loadStar(){
-    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}plane/`);
-        this.ready = false;
-        
-		// Load a glTF resource
-		loader.load(
-			// resource URL
-			'star.glb',
-			// called when the resource is loaded
-			gltf => {
+    this.obstacles.push(obstacle)
+    this.scene.add(obstacle)
 
-                this.star = gltf.scene.children[0];
-
-                this.star.name = 'star';
-
-				if (this.bomb !== undefined) this.initialize();
-
-			},
-			// called while loading is progressing
-			xhr => {
-
-                this.loadingBar.update('star', xhr.loaded, xhr.total );
-			
-			},
-			// called when loading has errors
-			err => {
-
-				console.error( err );
-
-			}
-		);
-	}	
-
-    loadBomb(){
-    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}plane/`);
-        
-		// Load a glTF resource
-		loader.load(
-			// resource URL
-			'bomb.glb',
-			// called when the resource is loaded
-			gltf => {
-
-                this.bomb = gltf.scene.children[0];
-
-                if (this.star !== undefined) this.initialize();
-
-			},
-			// called while loading is progressing
-			xhr => {
-
-				this.loadingBar.update('bomb', xhr.loaded, xhr.total );
-				
-			},
-			// called when loading has errors
-			err => {
-
-				console.error( err );
-
-			}
-		);
-	}
-
-	initialize(){
-        
+    for (let i = 0; i < 3; i++) {
+      const newObstacle = obstacle.clone()
+      this.scene.add(newObstacle)
+      this.obstacles.push(newObstacle)
     }
 
-    reset(){
-        
-    }
+    this.reset()
+    this.ready = true
+  }
 
-    respawnObstacle( obstacle ){
-        
-    }
+  reset() {
+    this.obstacleSpawn = { pos: 20, offset: 5 }
+    this.obstacles.forEach((obstacle) => this.respawnObstacle(obstacle))
+  }
 
-	update(pos){
-        
-    }
+  respawnObstacle(obstacle) {
+    this.obstacleSpawn.pos += 30
+    const offset = (Math.random() * 2 - 1) * this.obstacleSpawn.offset
+    this.obstacleSpawn.offset += 0.2
+    obstacle.position.set(0, offset, this.obstacleSpawn.pos)
+    obstacle.children[0].rotation.y = Math.random() * Math.PI * 2
+    obstacle.userData.hit = false
+    obstacle.children.forEach((child) => (child.visible = true))
+  }
 
-	hit(obj){
-		
-	}
+  update(pos) {}
+
+  hit(obj) {}
 }
 
-export { Obstacles };
+export { Obstacles }
